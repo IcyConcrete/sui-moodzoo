a7f3630af8ed9979adb1e7db46be0e271fbbeb24
IcyConcrete <2556553872@qq.com>
2025-05-24 11:48:50 +0800

    fix

--- frontend/app/details/[id]/page.tsx
+++ frontend/app/details/[id]/page.tsx
@@ -1,13 +1,7 @@
 import NFTDetailPageClient from './page.client';
 
-// 异步页面组件
-export default async function Page({params}: {params: Promise<{ id: string }>}) {
-  // 获取路由参数
-  const { id } = await params;
+export default function Page({params}: {params: { id: string }}) {
+  const { id } = params;
   
-  // 这里可以异步获取 NFT 数据
-  // 例如: const nftData = await fetchNFTData(id);
-  
-  // 渲染客户端组件
   return <NFTDetailPageClient id={id} />;
 }
